67b53db0385f32c38698c4585fc362820ca26ee9
jacquetta <jacquettavb@hotmail.com>
2018-07-21 02:10:20 -0500

    updated all files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 /*
  * Create a list that holds all of your cards
  */
+const deck = document.querySelector('.deck');
+let toggledCards = [];
+let moves = 0;
+let clockOff = true;
+let time = 0;
+let clockId;
 
+function shuffleDeck() {
+  const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+  const shuffledCards = shuffle(cardsToShuffle);
+  for(card of shuffledCards){
+    deck.appendChild(card);
+  }
+}
+
+shuffleDeck();
 
 /*
  * Display the cards on the page
@@ -26,6 +41,115 @@ function shuffle(array) {
 }
 
 
+deck.addEventListener('click', event => {
+  const clickTarget = event.target;
+  if(isClickValid(clickTarget)){
+    if(clockOff){
+      startClock();
+      clockOff = false;
+    }
+    toggleCard(clickTarget);
+    addToggleCard(clickTarget);
+    if(toggledCards.length === 2){
+      checkForMatch(clickTarget);
+      addMove();
+      checkScore();
+    }
+  }
+});
+
+function toggleCard(card){
+  card.classList.toggle('open');
+  card.classList.toggle('show');
+}
+
+function addToggleCard(clickTarget){
+  toggledCards.push(clickTarget);
+  console.log(toggledCards);
+}
+
+function checkForMatch(){
+  if(
+    toggledCards[0].firstElementChild.className === toggledCards[1].firstElementChild.className){
+      toggledCards[0].classList.toggle('match');
+      toggledCards[1].classList.toggle('match');
+      toggledCards = [];
+    } else {
+      setTimeout(() => {
+      toggleCard(toggledCards[0]);
+      toggleCard(toggledCards[1]);
+      toggledCards = [];
+    }, 1000);
+  }
+}
+
+function isClickValid(clickTarget){
+  return (
+    clickTarget.classList.contains('card')&&
+    !clickTarget.classList.contains('match') &&
+    toggledCards.length < 2 &&
+    !toggledCards.includes(clickTarget)
+  );
+}
+
+function addMove() {
+  moves++;
+  const movesText = document.querySelector('.moves');
+  movesText.innerHTML = moves;
+}
+
+function checkScore() {
+  if(moves === 16 || moves === 24)
+  {
+    removeStar();
+  }
+}
+
+function starHidden(){
+  const starList = document.querySelectorAll('.stars li');
+  for (star of starList){
+    if(star.style.display !== 'none') {
+      star.style.display = 'none';
+      break;
+    }
+  }
+}
+starHidden();
+starHidden();
+
+function displayTime() {
+  const clock = document.querySelector('.clock');
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  if(seconds < 10) {
+    clock.innerHTML = `${minutes}:0${seconds}`;
+  } else {
+    clock.innerHTML = `${minutes}:${seconds}`;
+  }
+  clock.innerHTML = time;
+}
+displayTime();
+
+function startClock() {
+  clockId = setInterval(() => {
+    time++;
+    displayTime();
+    console.log(time);
+  }, 1000);
+}
+
+startClock();
+
+
+function stopClock(){
+  clearInterval(clockId);
+}
+
+// function displayTime() {
+//   const clock = document.querySelector('.clock');
+//   console.log(clock);
+//   clock.innerHTML = time;
+// }
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
